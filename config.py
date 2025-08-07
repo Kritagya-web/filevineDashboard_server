e2605264a38657b14c6b812be226962bcf2fcad3
Kritagya-web <kritagyakumra@gmail.com>
2025-08-07 09:49:23 -0700

    Added gunicorn to requirements.txt

--- config.py
+++ config.py
@@ -1,12 +1,26 @@
 # config.py
+import os
+from dotenv import load_dotenv
+
+# if you're running outside of VS Code's debugger, explicitly load the .env:
+load_dotenv()  
 
 # Filevine API
-API_BASE_URL = "https://calljacob.api.filevineapp.com"
+API_BASE_URL = os.getenv("API_BASE_URL", "https://calljacob.api.filevineapp.com")
+
+API_KEY     = os.getenv("API_KEY")
+API_SECRET  = os.getenv("API_SECRET")
+USER_ID     = os.getenv("USER_ID")
+ORG_ID      = os.getenv("ORG_ID")
+SESSION_URL = os.getenv("SESSION_URL")
 
 # Postgres connection
-DB_USER     = "postgres"
-DB_PASSWORD = "kritagya"
-DB_HOST     = "localhost"
-DB_PORT     = "5432"
-DB_NAME     = "calljacob"
-DB_URL      = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DB_USER     = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST     = os.getenv("DB_HOST")
+DB_PORT     = os.getenv("DB_PORT")
+DB_NAME     = os.getenv("DB_NAME")
+DB_URL      = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
